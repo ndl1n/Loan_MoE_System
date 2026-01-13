0edcc0737e6074d9b801607d3541ab3a341d39d5
ndl1n <a0938391667@gmail.com>
2026-01-13 17:41:58 +0800

    add init of services

--- services/__init__.py
+++ services/__init__.py
@@ -3,7 +3,7 @@ Services Package
 共用服務模組
 """
 
-from .database import mongo_db
-from .rag_service import rag_engine
+from .database import mongo_db, MongoManager
+from .rag_service import rag_engine, RAGService
 
-__all__ = ['mongo_db', 'rag_engine']
+__all__ = ['mongo_db', 'MongoManager', 'rag_engine', 'RAGService']
